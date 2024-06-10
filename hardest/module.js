14fbc9436f53e84262150444657429aa975aba0d
wiglett <84410992+SirBaconThe3rd@users.noreply.github.com>
2024-06-10 15:22:45 -0300

    Added original files

---
+++ hardest/module.js
@@ -0,0 +1,209 @@
+export const completions=[
+    {
+      "name": "Deflective",
+      "imo": 1,
+      "aredlDifficulty": 4,
+      "date": "2023-10-13",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/kqZQ0rc6n9A",
+      "description": "this level killed me inside"
+    },
+    {
+      "name": "Prismatic Haze",
+      "imo": 2,
+      "aredlDifficulty": 3,
+      "date": "2023-09-16",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/3YfbaFRRZwE",
+      "description": "Fun level created by Gizbro and some creep"
+    },
+    {
+      "name": "Inflective",
+      "imo": 3,
+      "aredlDifficulty": 2,
+      "date": "2023-10-15",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/SOPujjSkAeM",
+      "description": "these spiders are fun i promise"
+    },
+    {
+      "name": "Semicolon 3",
+      "imo": 4,
+      "aredlDifficulty": 8,
+      "date": "2023-12-16",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/_OsrXmBjO5E",
+      "description": ""
+    },
+    {
+      "name": "Phonk Town",
+      "imo": 5,
+      "aredlDifficulty": 1,
+      "date": "2023-11-22",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/316hrizkczU",
+      "description": "Ranking is based on unrated demon list* Rebeat December 24th 2023 after original recording corrupted"
+    },
+    {
+      "name": "Reflective",
+      "imo": 6,
+      "aredlDifficulty": 5,
+      "date": "02/01/23",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/Uf3pEUz-HV4",
+      "description": "One of my favs <3 although fuck you for motivating me to play deflective after"
+    },
+    {
+      "name": "Reverence",
+      "imo": 7,
+      "aredlDifficulty": 7,
+      "date": "2023-08-23",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/IJu-qrsftRo",
+      "description": ""
+    },
+    {
+      "name": "Sephtis",
+      "imo": 8,
+      "aredlDifficulty": 13,
+      "date": "2023-08-28",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/LoeQd1Vj_Bg",
+      "description": ""
+    },
+    {
+      "name": "TROLLMACHINE",
+      "imo": 9,
+      "aredlDifficulty": 6,
+      "date": "07/04/23",
+      "type": "TRUE",
+      "video": "NA",
+      "description": "Recording of completion corrupted"
+    },
+    {
+      "name": "Cataclysm",
+      "imo": 10,
+      "aredlDifficulty": 10,
+      "date": "2024-03-29",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/zyw13NlSNk0",
+      "description": ""
+    },
+    {
+      "name": "Disruption",
+      "imo": 11,
+      "aredlDifficulty": 9,
+      "date": "2023-09-01",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/2CQr14NOO2g",
+      "description": "Rebeat February 4th 2024 after original recording corrupted"
+    },
+    {
+      "name": "Penombre",
+      "imo": 12,
+      "aredlDifficulty": 11,
+      "date": "2023-08-19",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/R-3nRMABdJc",
+      "description": ""
+    },
+    {
+      "name": "Niwa",
+      "imo": 13,
+      "aredlDifficulty": 14,
+      "date": "03/12/23",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/QAWbnnUd3z4",
+      "description": ""
+    },
+    {
+      "name": "Precipitance",
+      "imo": 14,
+      "aredlDifficulty": 16,
+      "date": "2024-01-13",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/_RTHMG_U0to",
+      "description": ""
+    },
+    {
+      "name": "Lost Love",
+      "imo": 15,
+      "aredlDifficulty": 12,
+      "date": "05/03/24",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/D4HwSEWZpLA",
+      "description": ""
+    },
+    {
+      "name": "Penumbra",
+      "imo": 16,
+      "aredlDifficulty": 19,
+      "date": "02/12/23",
+      "type": "FALSE",
+      "video": "https://www.youtube.com/embed/hfaPtPdnTNs",
+      "description": ""
+    },
+    {
+      "name": "R3d Room",
+      "imo": 17,
+      "aredlDifficulty": "NA",
+      "date": "2023-12-28",
+      "type": "FALSE",
+      "video": "https://www.youtube.com/embed/m6LD52aAqdc",
+      "description": "No rating for aredl since it is on no lists*"
+    },
+    {
+      "name": "Acu",
+      "imo": 18,
+      "aredlDifficulty": 15,
+      "date": "2022-12-29",
+      "type": "TRUE",
+      "video": "https://www.youtube.com/embed/Vn-QAaUTWhA",
+      "description": ""
+    },
+    {
+      "name": "Thanatophobia",
+      "imo": 19,
+      "aredlDifficulty": 18,
+      "date": "04/12/22",
+      "type": "FALSE",
+      "video": "https://www.youtube.com/embed/vTdWlbenymY",
+      "description": ""
+    },
+    {
+      "name": "The End",
+      "imo": 20,
+      "aredlDifficulty": 17,
+      "date": "NA",
+      "type": "FALSE",
+      "video": "NA",
+      "description": ""
+    },
+    {
+      "name": "Swarm",
+      "imo": 21,
+      "aredlDifficulty": 20,
+      "date": "2023-12-14",
+      "type": "FALSE",
+      "video": "https://www.youtube.com/embed/vKfVpXwWTEo",
+      "description": ""
+    },
+    {
+      "name": "Leyak",
+      "imo": 22,
+      "aredlDifficulty": 21,
+      "date": "2023-11-13",
+      "type": "FALSE",
+      "video": "NA",
+      "description": ""
+    },
+    {
+      "name": "Arcane",
+      "imo": 23,
+      "aredlDifficulty": 22,
+      "date": "NA",
+      "type": "FALSE",
+      "video": "NA",
+      "description": ""
+    }
+  ]
---
+++ hardest/scripts.js
@@ -0,0 +1,343 @@
+const completions = [
+  {
+    "name": "Deflective",
+    "imo": 1,
+    "aredlDifficulty": 4,
+    "date": "2023-10-13",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/kqZQ0rc6n9A",
+    "description": "this level killed me inside"
+  },
+  {
+    "name": "Prismatic Haze",
+    "imo": 2,
+    "aredlDifficulty": 3,
+    "date": "2023-09-16",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/3YfbaFRRZwE",
+    "description": "Fun level created by Gizbro and some creep"
+  },
+  {
+    "name": "Inflective",
+    "imo": 3,
+    "aredlDifficulty": 2,
+    "date": "2023-10-15",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/SOPujjSkAeM",
+    "description": "these spiders are fun i promise"
+  },
+  {
+    "name": "Semicolon 3",
+    "imo": 4,
+    "aredlDifficulty": 8,
+    "date": "2023-12-16",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/_OsrXmBjO5E",
+    "description": ""
+  },
+  {
+    "name": "Phonk Town",
+    "imo": 5,
+    "aredlDifficulty": 1,
+    "date": "2023-11-22",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/316hrizkczU",
+    "description": "Ranking is based on unrated demon list*<br> Rebeat December 24th 2023 after original recording corrupted"
+  },
+  {
+    "name": "Reflective",
+    "imo": 6,
+    "aredlDifficulty": 5,
+    "date": "02/01/23",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/Uf3pEUz-HV4",
+    "description": "One of my favs <3 although fuck you for motivating me to play deflective after"
+  },
+  {
+    "name": "Reverence",
+    "imo": 7,
+    "aredlDifficulty": 7,
+    "date": "2023-08-23",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/IJu-qrsftRo",
+    "description": ""
+  },
+  {
+    "name": "Sephtis",
+    "imo": 8,
+    "aredlDifficulty": 13,
+    "date": "2023-08-28",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/LoeQd1Vj_Bg",
+    "description": ""
+  },
+  {
+    "name": "TROLLMACHINE",
+    "imo": 9,
+    "aredlDifficulty": 6,
+    "date": "07/04/23",
+    "type": "TRUE",
+    "video": "NA",
+    "description": "Recording of completion corrupted"
+  },
+  {
+    "name": "Cataclysm",
+    "imo": 10,
+    "aredlDifficulty": 10,
+    "date": "2024-03-29",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/zyw13NlSNk0",
+    "description": ""
+  },
+  {
+    "name": "Disruption",
+    "imo": 11,
+    "aredlDifficulty": 9,
+    "date": "2023-09-01",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/2CQr14NOO2g",
+    "description": "Rebeat February 4th 2024 after original recording corrupted"
+  },
+  {
+    "name": "Penombre",
+    "imo": 12,
+    "aredlDifficulty": 11,
+    "date": "2023-08-19",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/R-3nRMABdJc",
+    "description": ""
+  },
+  {
+    "name": "Niwa",
+    "imo": 13,
+    "aredlDifficulty": 14,
+    "date": "03/12/23",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/QAWbnnUd3z4",
+    "description": ""
+  },
+  {
+    "name": "Precipitance",
+    "imo": 14,
+    "aredlDifficulty": 16,
+    "date": "2024-01-13",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/_RTHMG_U0to",
+    "description": ""
+  },
+  {
+    "name": "Lost Love",
+    "imo": 15,
+    "aredlDifficulty": 12,
+    "date": "05/03/24",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/D4HwSEWZpLA",
+    "description": ""
+  },
+  {
+    "name": "Penumbra",
+    "imo": 16,
+    "aredlDifficulty": 19,
+    "date": "02/12/23",
+    "type": "FALSE",
+    "video": "https://www.youtube.com/embed/hfaPtPdnTNs",
+    "description": ""
+  },
+  {
+    "name": "R3d Room",
+    "imo": 17,
+    "aredlDifficulty": "NA",
+    "date": "2023-12-28",
+    "type": "FALSE",
+    "video": "https://www.youtube.com/embed/m6LD52aAqdc",
+    "description": "No rating for aredl since it is on no lists*"
+  },
+  {
+    "name": "Acu",
+    "imo": 18,
+    "aredlDifficulty": 15,
+    "date": "2022-12-29",
+    "type": "TRUE",
+    "video": "https://www.youtube.com/embed/Vn-QAaUTWhA",
+    "description": ""
+  },
+  {
+    "name": "Thanatophobia",
+    "imo": 19,
+    "aredlDifficulty": 18,
+    "date": "04/12/22",
+    "type": "FALSE",
+    "video": "https://www.youtube.com/embed/vTdWlbenymY",
+    "description": ""
+  },
+  {
+    "name": "The End",
+    "imo": 20,
+    "aredlDifficulty": 17,
+    "date": "NA",
+    "type": "FALSE",
+    "video": "NA",
+    "description": ""
+  },
+  {
+    "name": "Swarm",
+    "imo": 21,
+    "aredlDifficulty": 20,
+    "date": "2023-12-14",
+    "type": "FALSE",
+    "video": "https://www.youtube.com/embed/vKfVpXwWTEo",
+    "description": ""
+  },
+  {
+    "name": "Leyak",
+    "imo": 22,
+    "aredlDifficulty": 21,
+    "date": "2023-11-13",
+    "type": "FALSE",
+    "video": "NA",
+    "description": ""
+  },
+  {
+    "name": "Arcane",
+    "imo": 23,
+    "aredlDifficulty": 22,
+    "date": "NA",
+    "type": "FALSE",
+    "video": "NA",
+    "description": ""
+  }
+]
+let filteredCompletions = completions;
+let currentDifficulty = 'imo';
+//let filterRated = false;
+
+function renderList(items) {
+    const list = document.getElementById("completionList");
+    list.innerHTML = '';
+    items.forEach(item => {
+        const difficulty = item[currentDifficulty];
+        if (document.getElementById('sortCriteria').value=='hard') {
+          if (item.video !== "NA") {
+              // Extract video ID from embed link
+              const videoId = extractVideoId(item.video);
+              // Construct thumbnail URL
+              const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+              const watchUrl = `https://www.youtube.com/watch?v=${videoId}`;
+              const listItem = document.createElement("li");
+              listItem.innerHTML = `
+                  <div>
+                      <img src="${thumbnailUrl}" alt="Thumbnail" class="video-thumbnail" data-video="${watchUrl}" style="width: 300px; height: 169px;">
+                  </div>
+                  <div class="info">
+                      <h3>${item.name}</h3>
+                      <p>Placement: ${difficulty} - Date: ${item.date}</p>
+                      <p>${item.description}</p>
+                  </div>
+              `;
+              list.appendChild(listItem);
+          } else {
+              const listItem = document.createElement("li");
+              listItem.innerHTML = `
+                  <div class="noBg">
+                      <h3>${item.name}</h3>
+                      <p>Difficulty: ${difficulty} - Date: ${item.date}</p>
+                      <p>${item.description}</p>
+                  </div>
+              `;
+              list.appendChild(listItem);
+          }
+        } else {
+          if (item.video !== "NA") {
+            // Extract video ID from embed link
+            const videoId = extractVideoId(item.video);
+            // Construct thumbnail URL
+            const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+            const watchUrl = `https://www.youtube.com/watch?v=${videoId}`;
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `
+                <div>
+                    <img src="${thumbnailUrl}" alt="Thumbnail" class="video-thumbnail" data-video="${watchUrl}" style="width: 300px; height: 169px;">
+                </div>
+                <div class="info">
+                    <h3>${item.name}</h3>
+                    <p>Date: ${item.date}</p>
+                    <p>${item.description}</p>
+                </div>
+            `;
+            list.appendChild(listItem);
+          } else {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `
+                <div class="noBg">
+                    <h3>${item.name}</h3>
+                    <p>Date: ${item.date}</p>
+                    <p>${item.description}</p>
+                </div>
+            `;
+            list.appendChild(listItem);
+        }
+        }
+    });
+    const thumbnails = document.querySelectorAll('.video-thumbnail');
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            const videoUrl = thumbnail.getAttribute('data-video');
+            window.open(videoUrl, '_blank');
+        });
+    });
+}
+function extractVideoId(embedLink) {
+    const regExp = /(?:\/|v=)([a-zA-Z0-9_-]{11})(?:\?|&|$)/;
+    const match = embedLink.match(regExp);
+    if (match && match[1]) {
+        return match[1];
+    } else {
+        return null;
+    }
+}
+
+
+
+function sortList(criteria) {
+    filteredCompletions.sort((a, b) => {
+        if (criteria === 'hard') {
+            const diffA = a[currentDifficulty];
+            const diffB = b[currentDifficulty];
+            document.getElementById("difficultyDropdown").disabled = false;
+            return diffA - diffB;
+        } else if (criteria === 'dateMostRecent') {
+            document.getElementById("difficultyDropdown").disabled = true;
+            return new Date(b.date) - new Date(a.date);
+            
+        }
+        else if (criteria === 'dateOldest') {
+            document.getElementById("difficultyDropdown").disabled = true;
+            return new Date(a.date) - new Date(b.date);
+        }
+    });
+    renderList(filteredCompletions);
+}
+
+function toggleFilter() {
+    //filterRated = !filterRated;
+    if (document.getElementById('filterRated').checked) {
+        filteredCompletions = completions.filter(item => item.type === 'TRUE');
+    } else {
+        filteredCompletions = completions;
+    }
+    renderList(filteredCompletions);
+    //document.getElementById('sortCriteria').getElementsByTagName('option')[0].selected = true;
+    //document.getElementById('difficultyDropdown').getElementsByTagName('option')[0].selected = true;
+}
+
+function changeDifficulty() {
+    const difficultyDropdown = document.getElementById("difficultyDropdown");
+    currentDifficulty = difficultyDropdown.value;
+    renderList(filteredCompletions);
+    document.getElementById('sortCriteria').getElementsByTagName('option')[0].selected = true;
+    sortList('hard');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    renderList(filteredCompletions);
+});
