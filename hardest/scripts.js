66c18eca66003c38c5fd5f62ea622d300d74a4fe
vadd7 <vaddeszka2001@gmail.com>
2024-06-23 22:26:27 +0200

    fix shit
    IHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJSIHATEJS

--- hardest/scripts.js
+++ hardest/scripts.js
@@ -1,269 +1,42 @@
-const completions = [
-  {
-    "name": "Carcano",
-    "imo": 1,
-    "aredlDifficulty": 2,
-    "date": "2024-06-17",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/2AKkk0cEdik",
-    "description": "fun cognition style extreme :) new hardest"
-  },
-  {
-    "name": "Deflective",
-    "imo": 2,
-    "aredlDifficulty": 5,
-    "date": "2023-10-13",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/kqZQ0rc6n9A",
-    "description": "this level killed me inside"
-  },
-  {
-    "name": "Prismatic Haze",
-    "imo": 3,
-    "aredlDifficulty": 4,
-    "date": "2023-09-16",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/3YfbaFRRZwE",
-    "description": "Fun level created by Gizbro and some creep"
-  },
-  {
-    "name": "Inflective",
-    "imo": 4,
-    "aredlDifficulty": 3,
-    "date": "2023-10-15",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/SOPujjSkAeM",
-    "description": "these spiders are fun i promise"
-  },
-  {
-    "name": "Semicolon 3",
-    "imo": 5,
-    "aredlDifficulty": 9,
-    "date": "2023-12-16",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/_OsrXmBjO5E",
-    "description": "This level is insanely hard, I hate it so much"
-  },
-  {
-    "name": "Phonk Town",
-    "imo": 6,
-    "aredlDifficulty": 1,
-    "date": "2023-11-22",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/316hrizkczU",
-    "description": "Ranking is based on unrated demon list*<br> Rebeat December 24th 2023 after original recording corrupted"
-  },
-  {
-    "name": "Reflective",
-    "imo": 7,
-    "aredlDifficulty": 6,
-    "date": "2023-02-01",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/Uf3pEUz-HV4",
-    "description": "One of my favs <3 although fuck you for motivating me to play deflective after"
-  },
-  {
-    "name": "Reverence",
-    "imo": 8,
-    "aredlDifficulty": 8,
-    "date": "2023-08-23",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/IJu-qrsftRo",
-    "description": "Perfect extreme for beginners, beat this days after Penombre and just days before Sephtis"
-  },
-  {
-    "name": "Sephtis",
-    "imo": 9,
-    "aredlDifficulty": 14,
-    "date": "2023-08-28",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/LoeQd1Vj_Bg",
-    "description": "Really forgettable level but really fun, 3rd level I beat during that small time that kicked me forward in GD"
-  },
-  {
-    "name": "TROLLMACHINE",
-    "imo": 10,
-    "aredlDifficulty": 7,
-    "date": "2023-07-04",
-    "type": "TRUE",
-    "video": "NA",
-    "description": "Recording of completion corrupted, haven't rebeaten on recording since the gameplay is so janky"
-  },
-  {
-    "name": "Cataclysm",
-    "imo": 11,
-    "aredlDifficulty": 11,
-    "date": "2024-03-29",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/zyw13NlSNk0",
-    "description": "Died at the last ship an hour before 2.2 came out, got frustrated and went to bed, missed 2.2 launch."
-  },
-  {
-    "name": "Disruption",
-    "imo": 12,
-    "aredlDifficulty": 10,
-    "date": "2023-09-01",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/2CQr14NOO2g",
-    "description": "Originally fluked from 50% \\n Rebeat February 4th 2024 after original recording corrupted"
-  },
-  {
-    "name": "Penombre",
-    "imo": 13,
-    "aredlDifficulty": 12,
-    "date": "2023-08-19",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/R-3nRMABdJc",
-    "description": "This completion was the first of multiple to have been beaten within just a few days, after previous completions taking ages"
-  },
-  {
-    "name": "Niwa",
-    "imo": 14,
-    "aredlDifficulty": 15,
-    "date": "2023-03-12",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/QAWbnnUd3z4",
-    "description": "This took less than a day to beat and I remember being shocked how quickly I had beaten it"
-  },
-  {
-    "name": "Precipitance",
-    "imo": 15,
-    "aredlDifficulty": 17,
-    "date": "2024-01-13",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/_RTHMG_U0to",
-    "description": "Super consistent, banger song, banger gameplay, all around banger level"
-  },
-  {
-    "name": "Lost Love",
-    "imo": 16,
-    "aredlDifficulty": 13,
-    "date": "2024-05-03",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/D4HwSEWZpLA",
-    "description": "Cute little level I beat in about 4 hours for Maymory, was funner than I expected"
-  },
-  {
-    "name": "Penumbra",
-    "imo": 17,
-    "aredlDifficulty": 20,
-    "date": "2023-02-12",
-    "type": "FALSE",
-    "video": "https://www.youtube.com/embed/hfaPtPdnTNs",
-    "description": "This should really be an extreme"
-  },
-  {
-    "name": "R3d Room",
-    "imo": 18,
-    "aredlDifficulty": "NA",
-    "date": "2023-12-28",
-    "type": "FALSE",
-    "video": "https://www.youtube.com/embed/m6LD52aAqdc",
-    "description": "No rating for aredl since it is on no lists*"
-  },
-  {
-    "name": "Acu",
-    "imo": 19,
-    "aredlDifficulty": 16,
-    "date": "2022-12-29",
-    "type": "TRUE",
-    "video": "https://www.youtube.com/embed/Vn-QAaUTWhA",
-    "description": "First extreme I beat that is still an extreme"
-  },
-  {
-    "name": "Thanatophobia",
-    "imo": 20,
-    "aredlDifficulty": 19,
-    "date": "2022-04-12",
-    "type": "FALSE",
-    "video": "https://www.youtube.com/embed/vTdWlbenymY",
-    "description": "beat this when it was an extreme demon as a jump from (I think) Nine Circles"
-  },
-  {
-    "name": "The End",
-    "imo": 21,
-    "aredlDifficulty": 18,
-    "date": "NA",
-    "type": "FALSE",
-    "video": "NA",
-    "description": "one of the first hard levels i played, and one of the longest"
-  },
-  {
-    "name": "Swarm",
-    "imo": 22,
-    "aredlDifficulty": 21,
-    "date": "2023-12-14",
-    "type": "FALSE",
-    "video": "https://www.youtube.com/embed/vKfVpXwWTEo",
-    "description": "fun level :) fluked from 39 when it was still extreme"
-  },
-  {
-    "name": "Leyak",
-    "imo": 23,
-    "aredlDifficulty": 22,
-    "date": "2023-11-13",
-    "type": "FALSE",
-    "video": "NA",
-    "description": "A bit overrated but still good"
-  },
-  {
-    "name": "Arcane",
-    "imo": 24,
-    "aredlDifficulty": 23,
-    "date": "NA",
-    "type": "FALSE",
-    "video": "NA",
-    "description": "Forgettable but fun"
-  },
-  {
-    "name": "My Time",
-    "imo": 25,
-    "aredlDifficulty": 24,
-    "date": "2024-06-16",
-    "type": "FALSE",
-    "video": "https://www.youtube.com/embed/SkmhJCAaygQ",
-    "description": "omori demon"
-  }
-]
-let filteredCompletions = completions;
-let currentDifficulty = 'imo';
+let completions;
+
 //let filterRated = false;
 
 function renderList(items) {
     const list = document.getElementById("completionList");
     list.innerHTML = '';
-    items.forEach(item => {
-        const difficulty = item[currentDifficulty];
-        if (document.getElementById('sortCriteria').value=='hard') {
-          if (item.video !== "NA") {
-              // Extract video ID from embed link
-              const videoId = extractVideoId(item.video);
-              // Construct thumbnail URL
-              const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-              const watchUrl = `https://www.youtube.com/watch?v=${videoId}`;
-              const listItem = document.createElement("li");
-              listItem.innerHTML = `
-                  <div>
-                      <img src="${thumbnailUrl}" alt="Thumbnail" class="video-thumbnail" data-video="${watchUrl}" style="width: 300px; height: 169px; cursor: pointer;">
-                  </div>
-                  <div class="info">
-                      <h3 class="linkName"><a href="${watchUrl}" style="text-decoration:none; color:inherit">${item.name}</a></h3>
-                      <p>Placement: ${difficulty} - Date: ${item.date}</p>
-                      <p>${item.description}</p>
-                  </div>
-              `;
-              list.appendChild(listItem);
-          } else {
-              const listItem = document.createElement("li");
-              listItem.innerHTML = `
-                  <div class="noBg">
-                      <h3>${item.name}</h3>
-                      <p>Difficulty: ${difficulty} - Date: ${item.date}</p>
-                      <p>${item.description}</p>
-                  </div>
-              `;
-              list.appendChild(listItem);
-          }
+	items.forEach((item) => { 
+		const difficulty = item[currentDifficulty];
+		if (document.getElementById('sortCriteria').value=='hard') {
+			if (item.video !== "NA") {
+			// Extract video ID from embed link
+			const videoId = extractVideoId(item.video);
+			// Construct thumbnail URL
+			const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+			const watchUrl = `https://www.youtube.com/watch?v=${videoId}`;
+			const listItem = document.createElement("li");
+			listItem.innerHTML = `
+				<div>
+					<img src="${thumbnailUrl}" alt="Thumbnail" class="video-thumbnail" data-video="${watchUrl}" style="width: 300px; height: 169px; cursor: pointer;">
+				</div>
+				<div class="info">
+					<h3 class="linkName"><a href="${watchUrl}" style="text-decoration:none; color:inherit">${item.name}</a></h3>
+					<p>Placement: ${difficulty} - Date: ${item.date}</p>
+					<p>${item.description}</p>
+				</div>
+			`;
+			list.appendChild(listItem);
+		} else {
+			const listItem = document.createElement("li");
+			listItem.innerHTML = `
+				<div class="noBg">
+					<h3>${item.name}</h3>
+					<p>Difficulty: ${difficulty} - Date: ${item.date}</p>
+					<p>${item.description}</p>
+				</div>
+			`;
+			list.appendChild(listItem);
+			}
         } else {
           if (item.video !== "NA") {
             // Extract video ID from embed link
@@ -296,6 +69,7 @@ function renderList(items) {
         }
         }
     });
+
     const thumbnails = document.querySelectorAll('.video-thumbnail');
     thumbnails.forEach(thumbnail => {
         thumbnail.addEventListener('click', () => {
@@ -357,22 +131,40 @@ function changeDifficulty() {
     sortList('hard');
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+
+fetch('./completions.json')
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		completions = data;
+        do_everything_else();
+	})
+    // WHO NEEDS ERROR HANDLING AM I RIGHT!!!!!!                                                                                                            :3
+	//.catch((error) => {
+	//	console.log('An error occurred:', error);
+	//});
+
+let filteredCompletions;
+let currentDifficulty;
+
+function do_everything_else() {
+    currentDifficulty = 'imo';
+    filteredCompletions = completions;
     renderList(filteredCompletions);
-});
 
+    var coll = document.getElementsByClassName("collapsible");
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var container = this.nextElementSibling;
-    if (container.style.maxHeight){
-      container.style.maxHeight = null;
-    } else {
-      container.style.maxHeight = container.scrollHeight + "px";
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var container = this.nextElementSibling;
+        if (container.style.maxHeight){
+            container.style.maxHeight = null;
+        } else {
+            container.style.maxHeight = container.scrollHeight + "px";
+        }
+    });
     }
-  });
+    
 }
